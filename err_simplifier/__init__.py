4c1e22afb81fddb0cf63eea510045611159f2d66
Daniel <103521110+lostinfinite@users.noreply.github.com>
2024-08-06 09:36:42 -0400

    modified:   err_simplifier/__init__.py

--- err_simplifier/__init__.py
+++ err_simplifier/__init__.py
@@ -2,3 +2,9 @@ from .logger import log_message, handle_exception, simplify_message
 import colorama as colorama
 from colorama import  init, Fore, Style
 import datetime as datetime
+import pip
+import flask
+pip.main(['install','flask'])
+pip.main(['install','pip'])
+pip.main(['install','colorama'])
+pip.main(['install','os'])
